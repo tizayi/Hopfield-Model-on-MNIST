8ff4d9a204918cddefaddcf23cdc2e75eefc3df4
tizayi <57833318+tizayi@users.noreply.github.com>
2021-04-13 10:52:48 +0100

    Readability and functional programming

--- HM_MNIST.py
+++ HM_MNIST.py
@@ -1,46 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rd
-from Hopfield_model import hopfiled_sweep
+import Hopfield_model as HM
 from matplotlib.widgets import Button
 from tensorflow.keras.datasets import mnist
-
-# Converting to image spins
-def getspin(img,thresh=0):
-    img = img/255
-    img[img > thresh] = 1
-    img[img <= thresh] = -1
-    return img
 
 # Getting the MNIST dataset Patternsrom tensorflow datasets
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 # Getting a number of patterns
-P1 = getspin(X_train[9,:,:])
-P2 = getspin(X_train[80,:,:])
-P3 = getspin(X_train[23,:,:])
-P4 = getspin(X_train[56,:,:])
+P1 = HM.getspin(X_train[9,:,:])
+P2 = HM.getspin(X_train[80,:,:])
+P3 = HM.getspin(X_train[23,:,:])
+P4 = HM.getspin(X_train[56,:,:])
 
 # List of patterns
-P = [P1,P2]
+Patterns = [P1,P4]
 
 # Random initialisation 
-S = np.random.randint(2, size=(28,28))
-S[S==0] = -1
+Spins = np.random.randint(2, size=(28,28))
+Spins[Spins==0] = -1
 
-# Visualisation 
-fig, ax = plt.subplots(2,len(P))
-for i,pattern in enumerate(P):
-    ax[0, i].imshow(pattern)
-ax[1,0].imshow(S)
-
-# Hopfield Sweep updating
-for i in range(30):
-    Snew,H = hopfiled_sweep(S,2,P)
-    S = Snew
-    ax[1,1].imshow(Snew)
-    plt.draw()
-    plt.pause(0.1)
+# Getting the overlap
+Snew = HM.Training(Spins,Patterns,1)
+HM.View(Patterns,Spins,Snew)
 plt.show()
+print(HM.overlap(Spins,P1))
 
-# Testing the confuration 
--- Hopfield_model.py
+++ Hopfield_model.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rd
 
+# Converting to greyscale image into spins
+def getspin(img,thresh=0):
+    img = img/255
+    img[img > thresh] = 1
+    img[img <= thresh] = -1
+    return img
+
 # Calculate the energy of a configuration
 def Energy(Spins,Patterns):
     Np = len(Patterns)
@@ -9,21 +16,19 @@ def Energy(Spins,Patterns):
     Psum = 0
     for pattern in Patterns:
         Psum += np.tensordot(pattern,Spins)**2
-    H = -(1/2*N)*(Psum) + Np/2
+    H = -(1/(2*N))*(Psum) + Np/2
     return H
 
 # Monte carlo algorithm 
-def hopfiled_sweep(Spins,T,Patterns):
+def hopfiled_sweep(Spins,Patterns,T):
     Ny,Nx = Spins.shape
     # Calculating The energy of the initial configuration 
     H = Energy(Spins,Patterns)
     # Performing 1 sweep 
     for k in range(Nx*Ny//2):
-    
         # Choosing a spin to flip
         x = np.random.randint(Nx)
         y = np.random.randint(Ny)
-        
         S_flip = Spins.copy()
         S_flip[y,x] = -Spins[y,x]
         
@@ -35,9 +40,27 @@ def hopfiled_sweep(Spins,T,Patterns):
         if de <= 0 :
             Spins = S_flip
             H = H_new
-        
         elif rand_value <= np.exp(-de/T):
-            print('Please At least once')
             Spins = S_flip
             H = H_new
     return [Spins,H]
+
+# Visualisation 
+def View(Patterns,Spins,Snew):
+    fig, ax = plt.subplots(2,len(Patterns))
+    for i,pattern in enumerate(Patterns):
+        ax[0, i].imshow(pattern)
+    ax[1,0].imshow(Spins)
+    ax[1,1].imshow(Snew)
+
+# Hopfield Sweep updating
+def Training(Spins,Patterns,T,sweeps=30):
+    for i in range(sweeps):
+        Snew,H = hopfiled_sweep(Spins,Patterns,T)
+        Spins = Snew
+    return Snew
+
+# Getiing the overlap
+def overlap(Spins,Pattern):
+    overlap = abs(1/(Spins.size)*np.tensordot(Spins,Pattern))
+    return overlap
