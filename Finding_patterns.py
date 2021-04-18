6033c2fb2706cb59d305617899281cdbb5cb19c2
tizayi <57833318+tizayi@users.noreply.github.com>
2021-04-18 10:12:50 +0100

    plotting patterns

--- Finding_patterns.py
+++ Finding_patterns.py
@@ -6,7 +6,15 @@ from tensorflow.keras.datasets import mnist
 # Getting the MNIST dataset Patternsrom tensorflow datasets
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
+# Creating matrix of indices 
+idxmat=np.zeros((10,10))
+for i in range(10):
+    idxmat[i,:] = np.random.choice(np.where(Y_train==i)[0],10)
 
-idx = np.random.choice(np.where(Y_train==1)[0],10)
-P = X_train[9,:,:]
-plt.imshow(P)
+# Plotting samples
+fig, ax = plt.subplots(10,10,figsize=(8,8))
+for i in range(10):
+    for j in range(10):
+        P = X_train[int(idxmat[i,j]),:,:]
+        ax[i,j].imshow(P)
+plt.show()
