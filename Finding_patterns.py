97356bfc595a9d93a0d2c3f666f100dd5f9c4dd6
tizayi <57833318+tizayi@users.noreply.github.com>
2021-04-26 23:28:19 +0100

    patterns finding works

--- Finding_patterns.py
+++ Finding_patterns.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import Hopfield_model as HM
 from tensorflow.keras.datasets import mnist
 
-np.random.seed(2)
+np.random.seed(7)
 
 # Getting the MNIST dataset Patternsrom tensorflow datasets
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 # Creating matrix of indices 
-idxmat=np.zeros((10,10))
+idxmat = np.zeros((10,10))
 for i in range(10):
     idxmat[i,:] = np.random.choice(np.where(Y_train==i)[0],10)
 
@@ -17,6 +17,7 @@ for i in range(10):
 def get_loss(i,j,idxmat,X_train):
     P1 = HM.getspin(X_train[int(idxmat[i,j]),:,:],thresh=0)
     # Similarity with the same number 
+    
     same_num = 0
     for k in range(len(idxmat[i,:])):
         P2 = HM.getspin(X_train[int(idxmat[i,k]),:,:],thresh=0)
@@ -28,12 +29,12 @@ def get_loss(i,j,idxmat,X_train):
         for n in range(len(idxmat[:,j])):
             if m != i:
                 P2 = HM.getspin(X_train[int(idxmat[m,n]),:,:],thresh=0) 
-                dif_num += (1/(len(idxmat[:,j])-1))*np.tensordot(P1,P2)**2
+                dif_num += np.tensordot(P1,P2)**2
     
-    return same_num-dif_num
+    return dif_num-10*same_num
 
 # Plotting samples
-lossspace=np.zeros((10,10))
+lossspace = np.zeros((10,10))
 
 for i in range(10):
     for j in range(10):
@@ -44,7 +45,6 @@ for i in range(10):
 best_idx = np.zeros(10) 
 for y in range(len(idxmat[i,:])):
     best_idx[y] = np.argmin(lossspace[y,:])
-print(best_idx)
 
 fig, ax = plt.subplots(10,10,figsize=(8,8))
 for i in range(10):
@@ -54,5 +54,5 @@ for i in range(10):
         ax[i,j].set_xticks([]) 
         ax[i,j].set_yticks([])
         p = int(best_idx[i])
-        ax[i,p].title.set_text('best')
+        ax[i,p].title.set_text('BEST')
 plt.show()
