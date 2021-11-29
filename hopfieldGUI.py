0b746bfe67e57473d29ba1dffebb85bf0f150b5a
tizayi <57833318+tizayi@users.noreply.github.com>
2021-11-29 19:09:25 +0000

    Update hopfieldGUI.py

--- hopfieldGUI.py
+++ hopfieldGUI.py
@@ -1,3 +1,4 @@
+from struct import pack
 from Hopfield_model import HopfieldModel,pseudoInverseModel
 from tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -9,23 +10,28 @@ import numpy as np
 
 class MyWindow:
     def __init__(self, win):
-        self.model = pseudoInverseModel([3,6,7,5])
-        self.fig = Figure(figsize=(4.5, 3), dpi=100)
+        
+        self.model = HopfieldModel([3,6])
+        self.fig = Figure(figsize=(6, 6), dpi=100)
         self.axes = self.fig.add_subplot(1, 1, 1)
         self.axes.imshow(self.model.getState())
         
-        self.btn = Button(win, text='sweep')
+        self.btn = Button(win, text='Sweep-1x', height = 1, width = 10)
         self.btn.bind('<Button-1>', self.sweep)
-        self.btn.place(x=100, y=50 + 80)
+        self.btn.place(x=10, y=50)
 
-        self.btn = Button(win, text='Ramdomise')
+        self.btn = Button(win, text='Sweep-5x',height = 1, width = 10)
+        self.btn.bind('<Button-1>', self.multisweep)
+        self.btn.place(x=110, y=50)
+
+        self.btn = Button(win, text='Ramdomise',height = 1, width = 10)
         self.btn.bind('<Button-1>', self.random)
-        self.btn.place(x=100, y=100 + 80)
+        self.btn.place(x=110, y=80)
         
         # Add noise
-        self.btn = Button(win, text='Add noise')
+        self.btn = Button(win, text='Add Noise',height = 1, width = 10)
         self.btn.bind('<Button-1>', self.noise)
-        self.btn.place(x=100, y=150 + 80)
+        self.btn.place(x=10, y=80)
         
         # Radio 
 
@@ -48,10 +54,20 @@ class MyWindow:
         self.model.addNoise(0.1)
         self.axes.imshow(self.model.getState())
         self.plots.draw()
+    
+    def multisweep(self,event):
+        self.model.hopfieldSweep(5)
+        self.axes.imshow(self.model.getState())
+        self.plots.draw()
+
 
 
 window = Tk()
+T = Text(window, height=2, width=30)
+T.pack(side=TOP, fill=X)
+T.insert(END, "The Hopfield model can store and recall patterns. This example demonstrates this using the \n MNIST digit dataset")
 mywin = MyWindow(window)
+
 window.title('Hopfiled Model')
 window.geometry("800x600+10+10")
 window.mainloop()
